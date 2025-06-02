6e826cb02e96c0e3ad2d43b6f612c111c1eaf23e
Eli Case <elicase4@gmail.com>
2025-06-01 23:59:05 -0500

    edit: update binary parser utils

--- include/Parser/Utils.hpp
+++ include/Parser/Utils.hpp
@@ -8,17 +8,18 @@ namespace SlicingParser {
 	
 	namespace BinaryUtils {
 		
-		std::ifstream openFile(const std::string& filename){
+		// add endianess handling
+		inline std::ifstream openFile(const std::string& filename){
 			return std::ifstream{filename, std::ifstream::in | std::ifstream::binary};
 		}
 		
 		template <typename T>
-		void readValue(std::ifstream& input_file_stream, T* data_stream){
+		inline void readValue(std::ifstream& input_file_stream, T* data_stream){
 			input_file_stream.read(reinterpret_cast<char*>(data_stream), sizeof(T));
 		}
 
 		template <typename T>
-		void readArray(std::ifstream& input_file_stream, T* data_stream, size_t array_length){
+		inline void readArray(std::ifstream& input_file_stream, T* data_stream, size_t array_length){
 			size_t num_bytes = array_length*sizeof(T);
 			input_file_stream.read(reinterpret_cast<char*>(data_stream), num_bytes);
 		}
