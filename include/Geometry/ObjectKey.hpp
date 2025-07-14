6a1f2e0412be30a2233e7c77b4a2c85457d1ea3a
Eli Case <elicase4@gmail.com>
2025-07-14 00:55:50 -0500

    edit: hash debug

--- include/Geometry/ObjectKey.hpp
+++ include/Geometry/ObjectKey.hpp
@@ -4,6 +4,7 @@
 #include <algorithm>
 #include <cmath>
 #include <cstdint>
+#include <iostream>
 #include <vector>
 #include <unordered_map>
 
@@ -27,20 +28,23 @@ namespace Geometry{
 	
 }
 
-template <>
-struct std::hash<Geometry::Coordinate3D> {
-	size_t operator()(const Geometry::Coordinate3D& coord) const {
-		size_t h1 = std::hash<double>()(coord.x);
-		size_t h2 = std::hash<double>()(coord.y);
-		size_t h3 = std::hash<double>()(coord.z);
-		return (h1 ^ (h2 << 1)) ^ h3;
-	}
-};
+namespace std{
+	
+	template <>
+	struct hash<Geometry::Coordinate3D> {
+		size_t operator()(const Geometry::Coordinate3D& coord) const {
+			size_t h1 = std::hash<double>()(coord.x);
+			size_t h2 = std::hash<double>()(coord.y);
+			size_t h3 = std::hash<double>()(coord.z);
+			return (h1 ^ (h2 << 1)) ^ (h3 << 2);
+		}
+	};
+}
 
 namespace Geometry{
 	
 	template <typename T>
-	Coordinate3D toCoordinate3D(T* coord, const T hash_coord_tol = static_cast<T>(1e-5)){ 
+	Coordinate3D toCoordinate3D(const T* coord, const T hash_coord_tol = 1e-5f){ 
 		Coordinate3D coord3d = {(double) coord[0], (double) coord[1], (double) coord[2], (double) hash_coord_tol};
 		return coord3d;
 	}
@@ -48,11 +52,11 @@ namespace Geometry{
 	template <typename T>
 	uint32_t addCoordinate3D(T* coord, std::vector<T*>& model_vector, std::unordered_map<Coordinate3D, uint32_t>& model_map, const T hash_coord_tol = static_cast<T>(1e-5)){
 		Coordinate3D coord3d = toCoordinate3D<T>(coord, hash_coord_tol);
-		auto [it, inserted] = model_map.try_emplace(coord3d, model_vector.size());
-		if (inserted){
+		auto result = model_map.try_emplace(coord3d, model_vector.size());
+		if (result.second){
 			model_vector.push_back(coord);
 		}
-		return it->second;
+		return result.first->second;
 	}
 }
 
